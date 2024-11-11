4d470230f1bb2c241f7fddbdb2f317c2c5072171
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-11-11 10:20:58 +0000

    feat: :sparkles: Updated LSP because it was not working with older node version

--- paths/.generic.paths.zsh
+++ paths/.generic.paths.zsh
@@ -1,6 +1,7 @@
 # Path additions
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$(brew --prefix libtool)/libexec/gnubin:$PATH"
-export PATH="$(brew --prefix icu4c)/bin:$(brew --prefix icu4c)/sbin:$PATH"
+export PATH="$(brew --prefix icu4c)/bin:$PATH"
+export PATH="$(brew --prefix icu4c)/sbin:$PATH"
 export PATH="$(brew --prefix openssl@3.0)/bin:$PATH"
 export PATH="$(brew --prefix postgresql@12)/bin:$PATH"
